9977dbf07ca30e4f67ee00bb5ae38c274582960a
Ricardo Moya Garcia <Vallekano_85@MacBook-Pro-de-Ricardo.local>
2015-10-18 20:01:42 +0200

    finish pivot_table examples

--- Example_MovieLens.py
+++ Example_MovieLens.py
@@ -8,7 +8,7 @@ userHeader = ['user_id', 'gender', 'age', 'ocupation', 'zip']
 users = pd.read_table('dataSet/users.txt', engine='python', sep='::', header=None, names=userHeader)
 
 # print 10 first users
-print '# 10 first users: \n%s' %users[:10]
+print '# 10 first users: \n%s' % users[:10]
 print '\n==================================================================\n'
 
 
@@ -18,7 +18,7 @@ ratings = pd.read_table('dataSet/ratings.txt', engine='python', sep='::', header
 
 # Example 1: merge tables users + ratings by user_id field
 merger_ratings_users = pd.merge(users, ratings)
-print '# Merge tables users + ratings by user_id field \n%s' %merger_ratings_users[:10]
+print '# Merge tables users + ratings by user_id field \n%s' % merger_ratings_users[:10]
 print '\n==================================================================\n'
 
 
@@ -28,16 +28,16 @@ movies = pd.read_table('dataSet/movies.txt', engine='python', sep='::', header=N
 
 # Example 2: merge ratings + users + movies
 merge_ratings = pd.merge(pd.merge(users, ratings), movies)
-print '# Merge tables ratings + users + movies by user_id and movie_id fields \n%s' %merge_ratings[:10]
+print '# Merge tables ratings + users + movies by user_id and movie_id fields \n%s' % merge_ratings[:10]
 print '\n==================================================================\n'
 
 
 # Example 3: Info about specific position (ejem: position 1000)
 info1000 = merge_ratings.ix[1000]
-print 'Info of 1000 position of the table: \n%s' %info1000[:10]
+print 'Info of 1000 position of the table: \n%s' % info1000[:10]
 print '\n==================================================================\n'
 
 
 # Example 4: show specific columns
 ratings_info = merge_ratings[['user_id', 'title', 'rating']]
-print 'Show specific columns: \n%s' %ratings_info[:10]
+print 'Show specific columns: \n%s' % ratings_info[:10]
